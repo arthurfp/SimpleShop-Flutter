29cc48aa0b6df71e8a3e839359dfccf195fd60c3
Arthur Ferreira <fepi.arthur@gmail.com>
2021-12-02 00:00:00 -0300

    added product form support

---
+++ lib/pages/product_form_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProductFormPage extends StatefulWidget {
+  const ProductFormPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductFormPage> createState() => _ProductFormPageState();
+}
+
+class _ProductFormPageState extends State<ProductFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Formulário de Produto'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
